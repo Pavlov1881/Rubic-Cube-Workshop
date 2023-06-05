c09e2cd846512b007c0016a007f09a581fc225c7
Anatoli Pavlov <18app81@gmail.com>
2023-06-05 13:07:44 +0300

    Render Home View

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
+// настройка на handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views'); 
+
+// всяка заявка към "static" се да се насочва към "public"
 app.use('/static', express.static('public'));
+app.use('/images', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('index');
 });
 
 app.listen(5000, () => console.log('App is listening on port 5000...'));
