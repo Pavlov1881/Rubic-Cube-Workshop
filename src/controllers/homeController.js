321a907c2f37bc19a0280b268df36afc73c581d8
Anatoli Pavlov <18app81@gmail.com>
2023-06-05 16:42:31 +0300

    Dynamic render cubes from json file

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
+const cubes = require('../db.json');
+
 exports.index = (req, res) => {
-    res.render('index');
+    res.render('index', {cubes}); // при всяка заявка към '/home/index', отива в index.hbs, където чрез темплейта от "partials - cube", динамично рендерира наличните кубчета
 } 
 
 exports.about = (req, res) => {
