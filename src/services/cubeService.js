6f42e2b9d25a34e95129ac1982b3f1a394d0917f
Anatoli Pavlov <18app81@gmail.com>
2023-06-06 12:36:05 +0300

    Add incremented id to created cubes

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,14 +1,16 @@
 const fs = require('fs/promises');
 const path = require('path');
 
+
 const cubes = require('../db.json');  // взимаме JSON файла
+
+exports.getOne = (cubeId) => cubes[cubeId]; // приема cubeId и връща от масива с кубовете cubeId
 
 // експортираме SAVE функция, която да ползваме в контролера
 exports.save = (cube) => {
-    cubes.push(cube);  // запизваме кубчето във файла
+    cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
 
-    // презаписваме db.json файла и парсваме данните към стринг
-    return fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), { encoding: 'utf-8' });
+    let textData = JSON.stringify(cubes, '', 4);
+
+    return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' })
 }
-
-exports.getOne = (cubeId) => cubes[cubeId]; // приема cubeId и връща от масива с кубовете cubeId
